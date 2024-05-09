bc17a5c6e039413d764540c7ab2ca92070610df3
atad_name <atad_email@email.com>
2024-05-09 15:24:22 +0000

    Saved /hackernews-alex/storiesbyday.sql manually.

--- storiesbyday.sql
+++ storiesbyday.sql
@@ -1,1 +1,8 @@
-Error: Output DSL not found or unsupported. Please make sure to end the query with 'in <DSL>' where DSL is one of: DBT, LookML, SQL, LKML
+SELECT
+  EXTRACT(YEAR FROM post_date) AS year,
+  EXTRACT(MONTH FROM post_date) AS month,
+  word,
+  SUM(score) AS total_score
+FROM fh-bigquery.hackernews.stories AS stories
+GROUP BY 1, 2, 3
+ORDER BY year, month, total_score DESC
